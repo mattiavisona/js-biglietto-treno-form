aae25d62b4601eb3276f068fb8a1a38460a7edda
mattiavisona <mattiav2706@gmail.com>
2023-02-23 19:30:38 +0100

    new file html- add input and button css- none js- add let

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+// INIZIO ESERCIZIO
+
+//_______________________________________________________________________________
+
+/*     PRIMA PARTE
+
+- Chiedo all'utente quale è il suo nome
+- Chiedo all'utente quanti kilometri dovrà percorrere
+- Chiedo all'utente la sua età
+
+*/
+
+
+// Stabilisco il prezzo per km
+
+const prezzoPerKm = 0.21;
+
+
+// Stabilisco le variabili
+let passengerNameEl = document.getElementById("passengerName");
+let passengerKmEl = document.getElementById("passengerKm");
+let passengerAgeEl = document.getElementById("passengerAge");
